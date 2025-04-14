b7982db5667a440afd2d2ff456b2af2e61aaebc9
Gorbatenko-Julia <ellis.tom@internet.ru>
2025-04-14 16:12:10 +0300

    add new class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!");
         System.out.println("Second commit");
 
+        Cat firstCat = new Cat("Барсик", "Чёрный", 2);
+        Cat SecondCat = new Cat("Мурзик", "Белый", 1);
+
+        System.out.println( "Данные первого кота: " +  firstCat.getName() + " ," + firstCat.getAge()
+        + " ," + firstCat.getColor());
+        System.out.println( "Данные второго кота: " +  SecondCat.getName()+ " ," + SecondCat.getAge()
+                + " ," + SecondCat.getColor());
     }
 }
